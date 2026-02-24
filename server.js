97b70dfd18a1d3e47d252a738828765b172ce9ee
niltinho8 <niltintago@gmail.com>
2026-02-24 02:54:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,74 @@
 const express = require("express");
-const cors = require("cors");
 const fetch = require("node-fetch");
+const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
+//////////////////////////////////////////////////
+// PEGAR TOKEN AMADEUS
+//////////////////////////////////////////////////
+
+async function pegarToken() {
+
+const r = await fetch(
+"https://test.api.amadeus.com/v1/security/oauth2/token",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/x-www-form-urlencoded"
+},
+body:
+`grant_type=client_credentials&client_id=${process.env.AMADEUS_KEY}&client_secret=${process.env.AMADEUS_SECRET}`
+});
+
+const d = await r.json();
+
+return d.access_token;
+}
+
+//////////////////////////////////////////////////
+// BUSCAR VOOS
+//////////////////////////////////////////////////
+
+app.get("/voos", async (req,res)=>{
+
+try{
+
+const {
+origem,
+destino,
+data,
+classe
+}=req.query;
+
+const token = await pegarToken();
+
+const url =
+`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${origem}&destinationLocationCode=${destino}&departureDate=${data}&adults=1&travelClass=${classe}&max=5`;
+
+const r = await fetch(url,{
+headers:{Authorization:`Bearer ${token}`}
+});
+
+const dados = await r.json();
+
+res.json(dados);
+
+}catch(e){
+
+res.status(500).json({erro:"Erro ao buscar voos"});
+
+}
+
+});
+
+//////////////////////////////////////////////////
 
 const PORT = process.env.PORT || 3000;
 
-let token = "";
-
-async function getToken() {
-  const response = await fetch(
-    "https://test.api.amadeus.com/v1/security/oauth2/token",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `grant_type=client_credentials&client_id=${process.env.AMADEUS_CLIENT_ID}&client_secret=${process.env.AMADEUS_CLIENT_SECRET}`,
-    }
-  );
-
-  const data = await response.json();
-  token = data.access_token;
-}
-
-app.get("/voos", async (req, res) => {
-  try {
-    if (!token) await getToken();
-
-    const { origem, destino, data } = req.query;
-
-    const response = await fetch(
-      `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${origem}&destinationLocationCode=${destino}&departureDate=${data}&adults=1&max=5`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const resultado = await response.json();
-    res.json(resultado);
-  } catch (error) {
-    res.json({ erro: "Erro ao buscar voos" });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log("Servidor rodando");
+app.listen(PORT,()=>{
+console.log("Servidor rodando");
 });
